9fef57547094dc3fab2c6d781152d3d5ad5001bc
VKAmpah <vkampah28@gmail.com>
2024-01-15 00:28:19 -0330

    update navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,15 +5,17 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
   return (
-    <div className="hidden md:flex h-20 flex-row gap- justify-between z-50">
-      <div className="flex w-2/6 mr-40">
+    <div className="md:flex h-20 flex-row gap- justify-between z-50">
+      <div className="hidden md:flex w-2/6 mr-40">
         <Avatar className="rounded-full h-20 w-20">
-          <AvatarImage src="/assets/reactic.svg" />
+          <AvatarImage src="/images/profile2.jpg" />
           <AvatarFallback>VA</AvatarFallback>
         </Avatar>
       </div>
-      <div className="flex flex-row rounded-full p-1 m-1 brightness-90">
-        <h1 className="font-bold"> Valentine Ampah</h1>
+
+      <div className="flex flex-col rounded-full justify-center items-center p-1 m-1 brightness-90">
+        <h1 className="font-bold text-4xl"> Valentine Ampah</h1>
+        <p> Software Developer</p>
       
         {/* <ModeToggle /> */}
       </div>
